dea821daed7f108a3ccea3caf095cc0758746fbf
Abigail Chiaokhiao <abi.chi703@gmail.com>
2020-12-06 16:30:26 -0600

    Addition of work from 11/22

--- api/api.py
+++ api/api.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 import pandas as pd
-df = pd.read_csv(‘rest_info.csv’).dropna()
+df = pd.read_csv("rest_info.csv").dropna()
 @app.route('/restaurants/<id>')
 def get_rest(id):
-col = pd.read_csv('rest_info.csv', usecols = ['id'])
+    col = pd.read_csv("rest_info.csv", usecols = ['id'])
