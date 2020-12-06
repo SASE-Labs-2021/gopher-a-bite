36b736ffc2817947e9231c56575a8bf388b269e8
Joseph Pham <pham0231@umn.edu>
2020-12-06 16:51:47 -0600

    cleaned up file

--- src/Components/MyProfile.jsx
+++ src/Components/MyProfile.jsx
@@ -6,67 +6,24 @@ import CardColumns from 'react-bootstrap/CardColumns';
 
 class MyProfile extends Component {
     state = { 
-        // name: "Susan",
-        // birthday: "12/12/1998",
-        // restrictions: ["gluten-free", "vegan", "lactose", "nuts", "eggs", "milk"] ,
-            data: [
-                {
-                    name: "Susan",
-                    birthday: "12/12/1998",
-                    restrictions: ["gluten-free", "vegan", "lactose", "nuts", "eggs", "milk"] ,
-                },
-                {
-                    name: "tommy",
-                    birthday: "01/31/2002",
-                    restrictions: ["gluten"] ,
-                },
-                {
-                    name: "karen",
-                    birthday: "06/05/1974",
-                    restrictions: ["vegetarian"] ,
-                }
-            ]
-        }
-
-
-
-        // name: "susan",
-        // birthday: "12/12/1998",
-        // restrictions: "gluten, vegan" ,
-
-
-
-    // render() {
-    //     return (
-    //         <Card>
-    //             <ListGroup variant="flush">
-    //                 {/* Name */}
-    //                 <ListGroup.Item>
-    //                     <Card>Name: {this.state.name}</Card>
-    //                 </ListGroup.Item>
-    //                 {/* Birthday */}
-    //                 <ListGroup.Item>
-    //                     <Card>Birthday: {this.state.birthday}</Card>
-    //                 </ListGroup.Item>
-    //                 {/* Restrictions */}
-    //                 <ListGroup.Item>
-    //                     <Card>Restrictions:</Card>
-    //                 </ListGroup.Item>
-    //                     {/* empty */}
-    //                 <ListGroup.Item>
-    //                     <CardColumns>
-    //                         {this.state.restrictions.map(
-    //                             (restriction) => (
-    //                                 <Card style={{ width: '20rem' }}>{restriction}</Card>
-    //                             ),
-    //                         )}
-    //                     </CardColumns>
-    //                 </ListGroup.Item>
-    //             </ListGroup>
-    //             <Button variant="primary" size="lg">Edit Profile</Button>
-    //          </Card>
-    //     )
-    // }
+        data: [
+            {
+                name: "Susan",
+                birthday: "12/12/1998",
+                restrictions: ["gluten-free", "vegan", "lactose", "nuts", "eggs", "milk"] ,
+            },
+            {
+                name: "tommy",
+                birthday: "01/31/2002",
+                restrictions: ["gluten"] ,
+            },
+            {
+                name: "karen",
+                birthday: "06/05/1974",
+                restrictions: ["vegetarian"] ,
+            }
+        ]
+    }
     
     render() {
         return (
@@ -75,25 +32,18 @@ class MyProfile extends Component {
                     <Card>
                         <ListGroup variant="flush">
                             {/* Name */}
-                            <ListGroup.Item>
-                                <Card>Name: {profile.name}</Card>
-                            </ListGroup.Item>
+                            <ListGroup.Item> <Card>Name: {profile.name}</Card> </ListGroup.Item>
                             {/* Birthday */}
-                            <ListGroup.Item>
-                                <Card>Birthday: {profile.birthday}</Card>
-                            </ListGroup.Item>
+                            <ListGroup.Item> <Card>Birthday: {profile.birthday}</Card> </ListGroup.Item>
                             {/* Restrictions */}
-                            <ListGroup.Item>
-                                <Card>Restrictions:</Card>
-                            </ListGroup.Item>
+                            <ListGroup.Item> <Card>Restrictions:</Card> </ListGroup.Item>
                                 {/* empty */}
                             <ListGroup.Item>
                                 <CardColumns>
                                     {profile.restrictions.map(
                                         (restriction) => (
                                             <Card style={{ width: '20rem' }}>{restriction}</Card>
-                                        ),
-                                    )}
+                                        ))}
                                 </CardColumns>
                             </ListGroup.Item>
                         </ListGroup>
@@ -102,27 +52,6 @@ class MyProfile extends Component {
             ),
         )
     )}
-
-    
-
-
-
-
-    /*
-     * ORIGINAL
-    */
-
-    // render() { 
-    //     return ( 
-    //         <Card>
-    //         <ListGroup variant="flush">
-    //             <ListGroup.Item>Name: {this.state.name}</ListGroup.Item>
-    //             <ListGroup.Item>Birthday: {this.state.birthday}</ListGroup.Item>
-    //             <ListGroup.Item>Restrictions: {this.state.restrictions}</ListGroup.Item>
-    //         </ListGroup>
-    //         </Card>
-    //      );
-    // }
 }
  
 export default MyProfile;
