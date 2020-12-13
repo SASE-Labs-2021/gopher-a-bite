fb401ef1dd30206d9f6ded5c86acb1c2bfb0e010
Abigail Chiaokhiao <abi.chi703@gmail.com>
2020-12-13 17:56:38 -0600

    Revert "Conversion to json"
    This reverts commit fffd985572aa117ad5b6f80efaef769319e64025.

--- api/api.py
+++ api/api.py
@@ -1,18 +1,8 @@
 from flask import Flask
-import json
 
 app = Flask(__name__)
 import pandas as pd
 df = pd.read_csv("rest_info.csv").dropna()
-
 @app.route('/restaurants/<id>')
 def get_rest(id):
-    restRowLabels = df.loc[df.id==id].columns # Names/text 
-    restRowVals = df.loc[df.id==id].values.flatten() # flatten needed to make [[]] into []
-    dictObj = zip(restRowLabels,restRowVals)
-    jsonOut = json.dumps(dictObj, sort_keys=True)
-
-@app.route('/ids')
-def get_ids():
     col = pd.read_csv("rest_info.csv", usecols = ['id'])
-    # ^ not completed
