8ea45d0cf51708eacbd1ef1f2426dd7941e5d409
saisahil2015 <44451929+saisahil2015@users.noreply.github.com>
2021-03-07 16:51:55 -0600

    review and map updated

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export const config = {
+  // DO NOT USE THESE CREDENTIALS ! THEY ARE HERE TO HELP IN THE LEARNING PROCESS.
+  // ANY AND ALL DATA ON THAT DOMAIN IS SUBJECT TO CHANGE AND REMOVAL AT ANY TIME
+  // THIS ACCOUNT IS ALSO ON THE FREE PLAN AND IS SUBJECT TO RESTRICTIONS !
+ apiKey: "AIzaSyBks_6ZHVWFbOL36rxvpkKocTP4J3tJHzw",
+  authDomain: "gopher-a-bite.firebaseapp.com",
+  projectId: "gopher-a-bite",
+  storageBucket: "gopher-a-bite.appspot.com",
+  messagingSenderId: "909606262805",
+  appId: "1:909606262805:web:059cf08865e1bd0cdd961c",
+  measurementId: "G-08MZH5ZWQL"
+};
