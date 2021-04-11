3a9915be4c1817e140f44d292633c5e9f93759f3
Your Name <nathanielbd@gmail.com>
2021-04-11 00:11:17 -0500

    added restaurant info to preview and dummy rec_sys is highest rated

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Spinner from 'react-bootstrap/Spinner';
+import { getData } from '../Shared';
+import { Link } from 'react-router-dom';
 
 const white = {
     background: "#fff",
@@ -13,21 +16,67 @@ const white = {
 }
 class Intro extends Component 
 {
-    state = {  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            recommendations: [],
+            isLoading: true
+        }
+    }
+
+    async componentDidMount() {
+        const res = await getData('/recommendation/any_user') // change when real recommender system is implemented
+        console.log(res)
+        const recommendations = Object.keys(res['rating']).map(key => key)
+        recommendations.forEach(id => {
+            return fetch(`/restaurants/${id}`)
+                .then(res => res.json())
+                .then((resData) => {
+                    this.setState(
+                        (prevState) => {
+                            return {
+                                recommendations: prevState.recommendations.concat(resData),
+                                isLoading: false
+                            }
+                        }
+                    )
+                })
+        })
+        // this.setState({ recommendations: recommendations, isLoading: false })
+    }
+
     render() 
-    { 
+    {
+        // if (this.state.isLoading) {
+        //     return <Spinner/>
+        // } 
+
         return ( 
         <div className = 'align-items-center'>
             <Card style={white}>
             <Card.Body>
                 <Card.Title as ="h1">Welcome to Gopher A Bite!</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">created by the Society of Asian Scientist and Enginneers Lab team! AKA SASE Labs!</Card.Subtitle>
                 <Card.Text>
                     Are you a University of Minnesota, Twin Cities student? Do you love food? Well you're in the right place! 
-                    At Gopher A Bite, we give you a daily restaurant recommendation located near the UMN based on your reviews and profile!
+                    At Gopher A Bite, we give you a daily restaurant recommendation located near the UMN based on your reviews and the reviews of others!
                 </Card.Text>
-                <Button >Get My Own Personal Recommendations!</Button>
             </Card.Body>
+            </Card>
+            <Card style={white}>
+                <Card.Title as="h1">Recommendations</Card.Title>
+                {
+                    this.state.recommendations.length == 0 ? 
+                    <p>Not enough data to recommend you any restaurants! Try reviewing some restaurants and check back here.</p> :
+                    this.state.recommendations.map(json => {
+                        return (
+                            <div>
+                                <Card.Header as="h2">{json.restaurant}</Card.Header>
+                                <p>{json.desc}</p>
+                                <button><Link to={`/rest?restaurant=${json.restaurant}`}>See reviews</Link></button>
+                            </div>
+                        )
+                    })
+                }
             </Card>
         </div>
          );
