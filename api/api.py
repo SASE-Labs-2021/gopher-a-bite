c62f66398d96a8306353985dbc2665db10457f44
Your Name <nathanielbd@gmail.com>
2021-04-10 21:44:03 -0500

    add client side requests

--- api/api.py
+++ api/api.py
@@ -14,7 +14,7 @@ class Review(Resource):
         print(f'get: {REVIEWS[REVIEWS.rest_id == rest_id]}')
         print(f'rest_id: {rest_id}')
         print(f'{REVIEWS}')
-        return REVIEWS[REVIEWS.rest_id == rest_id].to_json(index=False, orient='records')
+        return json.dumps(json.loads(REVIEWS[REVIEWS.rest_id == rest_id].to_json(orient='records')), ensure_ascii=False)
 
     def put(self, rest_id):
         user_id = request.form['user_id']
@@ -23,14 +23,24 @@ class Review(Resource):
         new_row = dict(zip(columns, [
             rest_id, user_id, rating, review
         ]))
-        REVIEWS.loc[len(REVIEWS)] = new_row
+        replace_row = [[rest_id, user_id, rating, review]]
+        if not user_id in REVIEWS[REVIEWS.rest_id == rest_id].user_id.unique():
+            REVIEWS.loc[len(REVIEWS)] = new_row
+            print('added new row')
+        else:
+            REVIEWS.loc[(REVIEWS.rest_id == rest_id)&(REVIEWS.user_id == user_id)] = replace_row
+            print('replaced a row')
         print(f'put: {REVIEWS}')
         return new_row, 201
 
 api.add_resource(Review, '/review/<rest_id>')
 
 df = pd.read_csv("rest_info.csv").dropna() 
- 
+
+@app.route('/ratings/<id>')
+def get_rating(id):
+    return json.dumps({'rating': REVIEWS[REVIEWS.rest_id == id]['rating'].mean()}, sort_keys=False, ensure_ascii=False)
+
 @app.route('/restaurants/<id>') 
 def get_rest(id):
     restRowLabels = df.loc[df.id==id].columns # Grabs 1st row of titles
